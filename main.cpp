d94865d25b621e1ba3af01ccb310ad9fd0e6d630
bob(YurasovAO) <YurasovAO@mpei.ru>
2022-03-14 17:36:10 +0300

    боб.добавлен минимум

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ cout << "A + B = " << a + b << '\n'
      << "A / B = " << a / b << '\n';
      if(a>b)
         cout<<a;
-     else
+      else
+        cout<<b;
+      if(a<b)
+        cout<<a;
+      else
         cout<<b;
     return 0;
 }
